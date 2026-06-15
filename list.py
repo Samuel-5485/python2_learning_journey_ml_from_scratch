99417fb16caf58961917efc955ad19f622911cd9
Sami Diriba <samediriba54@gmail.com>
2026-06-15 18:27:05 +0300

    here some basic we need to know in the list

--- list.py
+++ list.py
@@ -9,3 +9,28 @@ print(prog_lang)
 
 #use in keyword to check whether an element exist or not
 print('Python' in prog_lang)
+
+#list nested inside list
+developer = ['Alice', 34, ['Rust', 'Python', 'C++']]
+print(developer[2])
+print(developer[2][1])
+
+#Unpacking values from a list method to assign value to variable
+developer = ['Jon', 12, 'Java developer']
+name, age, job = developer
+print(name)
+print(age)
+print(job)
+
+#If u need to collect any remaining elements from a list use asterist*
+name, *rest = developer
+print(name)
+print(rest)
+
+#Slicing we use operator :
+desserts = ['Cake', 'Cookies', 'Ice Cream', 'Pie', 'Brownies']
+desserts[1:4] # ['Cookies', 'Ice Cream', 'Pie']
+
+#If u wann extract even num
+numbers = [1, 2, 3, 4, 5, 6]
+numbers[1::2] # [2, 4, 6]
