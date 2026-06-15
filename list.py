64e104e88a52e96d90efa7a3fd4f3de9ce34491d
Sami Diriba <samediriba54@gmail.com>
2026-06-15 18:56:41 +0300

    working on list

--- list.py
+++ list.py
@@ -34,3 +34,62 @@ desserts[1:4] # ['Cookies', 'Ice Cream', 'Pie']
 #If u wann extract even num
 numbers = [1, 2, 3, 4, 5, 6]
 numbers[1::2] # [2, 4, 6]
+
+#append()
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers)
+
+#if u want to add list by following the previous list
+even_numbers = [7, 8, 9]
+numbers.append(even_numbers)
+print(numbers)
+
+#extend()
+numbers = [1,2,3,4,5]
+even_numbers = [6,7,8]
+numbers.extend(even_numbers)
+print(numbers)
+
+#insert(): you have to specify the index 
+numbers = [1,2,3]
+numbers.insert(2, 4)
+print(numbers)
+
+#remove():
+numbers.remove(1)
+print(numbers)
+
+numbers = [10, 20, 30, 40, 50, 50, 50]
+numbers.remove(50) #only the first occurence not all of them
+
+print(numbers)
+
+#pop(): remove the at specified index 
+numbers = [1,2,3,4] 
+numbers.pop(1)
+print(numbers)
+
+#clear(): when u need to empty the list
+numbers.clear()
+print(numbers)
+
+#sort()
+numbers = [43, 23,67, 22]
+numbers.sort()
+print(numbers)
+
+#sorted(): incontrast to sort()
+numbers = [43, 23,67, 22]
+sorted_numbers = sorted(numbers)
+print(numbers)
+print(sorted_numbers)
+
+#reverse()
+numbers.reverse()
+print(numbers)
+
+#use indext method to find elements
+programming_languages = ['Rust', 'Java', 'Python', 'C++']
+programming_languages.index('Java')
+print(programming_languages)
