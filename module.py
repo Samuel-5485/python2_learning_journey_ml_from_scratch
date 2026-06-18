40d594aac035a4b5d6c225aa66a8cbc9cb9fa8f8
Sami Diriba <samediriba54@gmail.com>
2026-06-18 16:48:33 +0300

    working with module

---
+++ module.py
@@ -0,0 +1,17 @@
+#from math import radians, sin, cos 
+import datetime
+birthday = datetime.date(2000, 4, 12)
+print(birthday.day)
+print(birthday.month)
+print(birthday.year)
+from math import * #we need call all function defined by this *
+print(sqrt(16))
+print(pow(5, 2))
+print(exp(1))
+
+angle_degrees = 50
+angle_radians = radians(angle_degrees)
+sine_value = sin(angle_degrees)
+cos_value = cos(angle_degrees)
+print(sine_value)
+print(cos_value)
